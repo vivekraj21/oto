24e506a1834b5bc2472ff4ea7b4ac0b56c37a6b2
vivekraj21 <vivekrajrollno21@gmail.com>
2025-04-04 23:00:16 +0530

    Adding ApplicationConfig class.

---
+++ call-management-app/src/main/java/com/project1/call_management_app/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.project1.call_management_app.config;
+
+import com.project1.call_management_app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
